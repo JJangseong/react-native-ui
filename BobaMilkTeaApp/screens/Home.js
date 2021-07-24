cc7c6c17cdd423164a9839c743bc6411eb6cb0c9
Jangseongjin <sungjin5891@gmail.com>
2021-07-24 18:28:25 +0900

    추가 : renderPromoDeals

--- BobaMilkTeaApp/screens/Home.js
+++ BobaMilkTeaApp/screens/Home.js
@@ -1,16 +1,135 @@
-import React from 'react';
+import React, {createRef, useCallback, useEffect, useRef, useState} from 'react';
 import {
     View,
-    StyleSheet, ScrollView, TouchableOpacity, ImageBackground, Text
+    StyleSheet, ScrollView, TouchableOpacity, ImageBackground, Text, Animated, Image
 } from 'react-native';
 
 import {connect} from 'react-redux'
 
-import {HeaderBar} from "../components";
+import {CustomButton, HeaderBar} from "../components";
 import {COLORS, SIZES, constants, icons, FONTS, dummyData, images} from "../constants";
-import {toggleTheme} from "../stores/themeActions";
+
+const promoTabs = constants.promoTabs.map((promoTab) => ({
+    ...promoTab,
+    ref: createRef()
+}))
+
+const TabIndicator = ({measureLayout, scrollX}) => {
+    const inputRange = promoTabs.map((_, i) => i * SIZES.width)
+
+    const tabIndicatorWidth = scrollX.interpolate({
+        inputRange,
+        outputRange: measureLayout.map(measure => measure.width)
+    })
+
+    const translateX = scrollX.interpolate({
+        inputRange,
+        outputRange: measureLayout.map(measure => measure.x)
+    })
+
+    return (
+        <Animated.View
+            style={{
+                position: 'absolute',
+                height: "100%",
+                width: tabIndicatorWidth,
+                left: 0,
+                borderRadius: SIZES.radius,
+                backgroundColor: COLORS.primary,
+                transform: [{
+                    translateX
+                }]
+            }}
+        />
+    )
+}
+
+const Tabs = ({appTheme, scrollX, onPromoTabPress}) => {
+
+    const [measureLayout, setMeasureLayout] = useState([])
+    const containerRef = useRef()
+
+    const tabPosition = Animated.divide(scrollX, SIZES.width)
+
+    useEffect(() => {
+        let ml = []
+
+        promoTabs.forEach((promo) => {
+            promo.ref.current.measureLayout(
+                containerRef.current,
+                (x, y, width, height) => {
+                    console.log(x, y, width, height)
+
+                    ml.push({
+                        x, y, width, height
+                    })
+
+                    if(ml.length === promoTabs.length){
+                        setMeasureLayout(ml)
+                    }
+                }
+            )
+        })
+    }, [containerRef.current])
+
+    return (
+        <View
+            ref={containerRef}
+            style={{
+                flexDirection: "row",
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginTop: SIZES.padding,
+                backgroundColor: appTheme.tabBackgroundColor,
+                borderRadius: SIZES.radius
+            }}
+        >
+            {measureLayout.length > 0 && <TabIndicator measureLayout={measureLayout} scrollX={scrollX} />}
+            {
+                promoTabs.map((item, index) => {
+
+                    const textColor = tabPosition.interpolate({
+                        inputRange: [index -1, index, index+1],
+                        outputRange: [COLORS.lightGray2, COLORS.white, COLORS.lightGray2],
+                        extrapolate: 'clamp'
+                    })
+
+                    return (
+                        <TouchableOpacity
+                            key={`PromoTab-${index}`}
+                            onPress={() => onPromoTabPress(index)}
+                        >
+                            <View
+                                ref={item.ref}
+                                style={{
+                                    paddingHorizontal: 15,
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    height: 40
+                                }}
+                            >
+                                <Animated.Text
+                                    style={{
+                                        color: textColor, ...FONTS.h3
+                                    }}
+                                >{item.title}</Animated.Text>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                })
+            }
+        </View>
+    )
+}
 
 const Home = ({navigation, appTheme}) => {
+    const scrollX = useRef(new Animated.Value(0)).current
+    const promoScrollViewRef = useRef()
+    const onPromoTabPress = useCallback((promoTabIndex) => {
+        promoScrollViewRef?.current?.scrollToOffset({
+            offset: promoTabIndex * SIZES.width
+        })
+    }, [])
 
     function renderAvailableRewards() {
         return (
@@ -108,6 +227,78 @@ const Home = ({navigation, appTheme}) => {
         )
     }
 
+    function renderPromoDeals() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center'
+                }}
+            >
+                <Tabs appTheme={appTheme} scrollX={scrollX} onPromoTabPress={onPromoTabPress}/>
+                <Animated.FlatList
+                    ref={promoScrollViewRef}
+                    data={dummyData.promos}
+                    horizontal
+                    pagingEnabled
+                    scrollEventThrottle={16}
+                    snapToAlignment="center"
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={item => `${item.id}`}
+                    onScroll={Animated.event([
+                        {nativeEvent: {contentOffset: {x: scrollX}}},
+                    ], {useNativeDriver: false})}
+                    renderItem={({item, index}) => {
+                        return (
+                            <View
+                                style={{
+                                    flex: 1,
+                                    alignItems: 'center',
+                                    width: SIZES.width,
+                                    paddingTop: SIZES.padding
+                                }}
+                            >
+                                <Image source={images.strawberryBackground}
+                                       resizeMode="contain"
+                                       style={{
+                                           width: "100%"
+                                       }}
+                                />
+
+                                <Text style={{color: COLORS.red, ...FONTS.h1, fontSize: 27}}>
+                                    {item.name}
+                                </Text>
+
+                                <Text style={{marginTop: 3, color: appTheme.textColor, ...FONTS.body4}}>
+                                    {item.description}
+                                </Text>
+
+                                <Text style={{marginTop: 3, color: appTheme.textColor, ...FONTS.body4}}>
+                                    Calories: {item.calories}
+                                </Text>
+
+                                <CustomButton
+                                    label="Order Now"
+                                    isPrimaryButton
+                                    containerStyle={{
+                                        marginTop: 10,
+                                        paddingHorizontal: SIZES.padding,
+                                        paddingVertical: SIZES.base,
+                                        borderRadius: SIZES.radius * 2
+                                    }}
+                                    labelStyle={{
+                                        ...FONTS.h3
+                                    }}
+                                    onPress={() => navigation.navigate("Location")}
+                                />
+                            </View>
+                        )
+                    }}
+                />
+            </View>
+        )
+    }
+
     return (
         <View style={styles.container}>
             <HeaderBar/>
@@ -125,6 +316,7 @@ const Home = ({navigation, appTheme}) => {
                 }}
             >
                 {renderAvailableRewards()}
+                {renderPromoDeals()}
             </ScrollView>
         </View>
     )
