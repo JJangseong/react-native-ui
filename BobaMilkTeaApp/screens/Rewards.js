7a4d0fbecf39c846a339936eeeccc84debdd7d76
Jangseongjin <sungjin5891@gmail.com>
2021-07-25 12:44:41 +0900

    추가 : Rewards 기본 레이아웃

--- BobaMilkTeaApp/screens/Rewards.js
+++ BobaMilkTeaApp/screens/Rewards.js
@@ -2,13 +2,56 @@ import React from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet, FlatList
 } from 'react-native';
 
-const Rewards = () => {
+import {dummyData, COLORS, FONTS, SIZES, icons} from '../constants'
+
+import {connect} from 'react-redux'
+import {HeaderBar} from "../components";
+
+const Rewards = ({navigation, appTheme}) => {
     return (
         <View style={styles.container}>
-            <Text>Rewards</Text>
+            <HeaderBar/>
+
+            <FlatList
+                style={{
+                    marginTop: -25,
+                    borderTopLeftRadius: SIZES.radius * 2,
+                    borderTopRightRadius: SIZES.radius * 2,
+                    backgroundColor: appTheme.backgroundColor
+                }}
+                data={dummyData.availableRewards}
+                keyExtractor={item => `${item.id}`}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View>
+                    </View>
+                }
+                renderItem={({item}) => {
+                    return (
+                        <View
+                            style={{
+                                alignItems: 'center',
+                                justifyContent: "center",
+                                marginHorizontal: SIZES.padding,
+                                marginBottom: SIZES.base,
+                                paddingVertical: SIZES.base,
+                                borderRadius: 20,
+                                backgroundColor: item.eligible ? COLORS.yellow : COLORS.gray2
+                            }}
+                        >
+                            <Text style={{
+                                color: item.eligible ? COLORS.black : COLORS.lightGray2,
+                                ...FONTS.body3
+                            }}>
+                                {item.title}
+                            </Text>
+                        </View>
+                    )
+                }}
+                />
         </View>
     )
 }
@@ -16,9 +59,18 @@ const Rewards = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
     }
 })
 
-export default Rewards;
+function mapStateToProps(state) {
+    return {
+        appTheme: state.appTheme,
+        error: state.error
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Rewards)
